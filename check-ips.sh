5ab251b833b0aa7df1174e65d187287309c5a925
Jay Lee <jay0lee@gmail.com>
2021-10-12 13:32:35 -0400

    first run

---
+++ check-ips.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while read line; do
+    url=$(echo $line | cut -d' ' -f1)
+    arrURL=(${url//// })
+    filename=${arrURL[-1]}
+    rightnow=$(date)
+    echo "Checking ${filename}..."
+    curl -s -f -o "${filename}" --compressed "${url}"; result=$?; true
+    if [ $result -ne 0 ]; then
+        echo "${url} failed with $result!"
+	continue
+    fi
+    jq --sort-keys . "${filename}" | tee "${filename}"
+done < ip-urls.txt
